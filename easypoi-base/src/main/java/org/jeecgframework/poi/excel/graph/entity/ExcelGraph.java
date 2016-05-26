b871a8de6e0c46887d843717d642f67f8d3cfa8b
xfworld <xf.key@163.com>
2016-05-26 14:24:29 +0800

    重新格式化版本，将原有图形化的服务恢复到默认状态

--- easypoi-base/src/main/java/org/jeecgframework/poi/excel/graph/entity/ExcelGraph.java
+++ easypoi-base/src/main/java/org/jeecgframework/poi/excel/graph/entity/ExcelGraph.java
@@ -9,17 +9,14 @@ import java.util.List;
  * @author xfworld
  * @since 2016-1-7
  * @version 1.0    
- * @see org.jeecgframework.poi.excel.graph.entity.ExcelGraph
+ * @see com.dawnpro.core.export.excel.model.ExcelGraph
  * 
  */
-public interface ExcelGraph {
-    public ExcelGraphElement getCategory();
-
-    public List<ExcelGraphElement> getValueList();
-
-    public Integer getGraphType();
-
-    public List<ExcelTitleCell> getTitleCell();
-
-    public List<String> getTitle();
+public interface ExcelGraph
+{
+	public ExcelGraphElement getCategory();
+	public List<ExcelGraphElement> getValueList();
+	public Integer getGraphType();
+	public List<ExcelTitleCell> getTitleCell();
+	public List<String> getTitle();
 }
