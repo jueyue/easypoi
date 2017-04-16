e41c71a85164575292d53dbac2c01203634edff9
jueyue <qrb.jueyue@gmail.com>
2017-04-16 17:33:29 +0800

    增加流输入的,html 导出

--- easypoi-base/src/main/java/org/jeecgframework/poi/exception/excel/enums/ExcelExportEnum.java
+++ easypoi-base/src/main/java/org/jeecgframework/poi/exception/excel/enums/ExcelExportEnum.java
@@ -24,6 +24,7 @@ public enum ExcelExportEnum {
 
         PARAMETER_ERROR ("Excel 导出   参数错误") ,
         EXPORT_ERROR ("Excel导出错误") ,
+        HTML_ERROR ("Excel导出Html流错误") ,
         TEMPLATE_ERROR ("Excel 模板错误");
 
     private String msg;
