e416a1f640a6511edab0b653991072d4983299eb
jueyue <qrb.jueyue@gmail.com@6760b078-30cd-4ec9-8538-5b9ad61545ab>
2018-04-08 03:44:54 +0000

    获取rownum接口

---
+++ easypoi-base/src/main/java/cn/afterturn/easypoi/handler/inter/IExcelDataModel.java
@@ -0,0 +1,21 @@
+package cn.afterturn.easypoi.handler.inter;
+
+/**
+ * Excel 本身数据文件
+ * @author by jueyue on 18-4-8.
+ */
+public interface IExcelDataModel {
+
+    /**
+     * 获取行号
+     * @return
+     */
+    public int getRowNum();
+
+    /**
+     *  设置行号
+     * @param rowNum
+     */
+    public void setRowNum(int rowNum);
+
+}
