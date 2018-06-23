bb5dd64e33d3b7cc3f91ec016de54f560caf6e50
jueyue <qrb.jueyue@gmail.com@6760b078-30cd-4ec9-8538-5b9ad61545ab>
2018-06-23 05:32:24 +0000

    删除坑爹缓存

--- easypoi-base/src/main/java/cn/afterturn/easypoi/cache/ImageCache.java
+++ easypoi-base/src/main/java/cn/afterturn/easypoi/cache/ImageCache.java
@@ -1,13 +1,13 @@
 /**
  * Copyright 2013-2015 JueYue (qrb.jueyue@gmail.com)
- *   
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -18,58 +18,43 @@ package cn.afterturn.easypoi.cache;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
 import org.apache.poi.util.IOUtils;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-
 import cn.afterturn.easypoi.cache.manager.POICacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 图片缓存处理
+ *
  * @author JueYue
- *  2016年1月8日 下午4:16:32
+ *         2016年1月8日 下午4:16:32
  */
 public class ImageCache {
 
-    private static LoadingCache<String, byte[]> loadingCache;
-
-    static {
-        loadingCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.DAYS)
-            .maximumSize(2000).build(new CacheLoader<String, byte[]>() {
-                @Override
-                public byte[] load(String imagePath) throws Exception {
-                    InputStream is = POICacheManager.getFile(imagePath);
-                    BufferedImage bufferImg = ImageIO.read(is);
-                    ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
-                    try {
-                        ImageIO.write(bufferImg,
-                            imagePath.substring(imagePath.indexOf(".") + 1, imagePath.length()),
-                            byteArrayOut);
-                        return byteArrayOut.toByteArray();
-                    } finally {
-                        IOUtils.closeQuietly(is);
-                        IOUtils.closeQuietly(byteArrayOut);
-                    }
-                }
-            });
-    }
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ImageCache.class);
 
     public static byte[] getImage(String imagePath) {
+        InputStream is = POICacheManager.getFile(imagePath);
+        ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
         try {
-            return loadingCache.get(imagePath);
+            BufferedImage bufferImg = ImageIO.read(is);
+            ImageIO.write(bufferImg,
+                    imagePath.substring(imagePath.indexOf(".") + 1, imagePath.length()),
+                    byteArrayOut);
+            return byteArrayOut.toByteArray();
         } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
             return null;
+        } finally {
+            IOUtils.closeQuietly(is);
+            IOUtils.closeQuietly(byteArrayOut);
         }
 
     }
-    
-    public static void setLoadingCache(LoadingCache<String, byte[]> loadingCache) {
-        ImageCache.loadingCache = loadingCache;
-    }
+
 }
